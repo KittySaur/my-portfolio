8da8d9ca56092e81beb8ed6ac6844a9aa8cd8be0
Zarina <zarina_nugmanova@mail.ru>
2022-09-05 20:37:00 -0400

    resume updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,8 +8,8 @@ function Home() {
         <h1>Zarina Nugmanova</h1>
         <h2>Frontend Developer</h2>
         <p>
-          An enthusiastic frontend developer looking for an entry level position
-          to apply my skills, share experience and benefit the company.
+          An enthusiastic frontend developer looking for an opportunity to apply
+          my skills, share experience and benefit the company.
         </p>
       </TextContainer>
     </Container>
